1f3078edd18ae2b951b64f3e4bf8fbfac5313d93
ashishnair436 <ashishnair436@gmail.com>
2023-04-03 22:07:00 +0530

    Refactor Code for New Input Manager
    Refactored old legacy code to new input manager for arrow keys, WASD and gamepad , will try for touch next.

--- Kitchen Chaos Game 1/Assets/Scripts/Player.cs
+++ Kitchen Chaos Game 1/Assets/Scripts/Player.cs
@@ -13,32 +13,15 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float rotateSpeed = 10f;
 
+    [SerializeField]
+    private GameInput gameInput;
+
     private bool isWalking;
 
     private void Update()
     {
 
-        Vector2 inputVector = new Vector2(0, 0);
-
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalised();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
